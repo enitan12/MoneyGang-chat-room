0cae700e4752b3db46d7b53e2cdfbda7c3dfa932
enitan12 <101717394+enitan12@users.noreply.github.com>
2023-11-16 00:13:13 +1200

    commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// start: sidebar //
 document.querySelector('.chat-sidebar-profile-toggle').addEventListener('click', function(e) {
     e.preventDefault()
     this.parentElement.classList.toggle('active')
@@ -8,3 +9,52 @@ document.addEventListener('click', function(e) {
         document.querySelector('.chat-sidebar-profile').classList.remove('active')
     }
 })
+//end: sidebar //
+
+// start: Conversation
+document.querySelectorAll('.conversation-item-dropdown-toggle').forEach(function(item) {
+    item.addEventListener('click', function(e) {
+        e.preventDefault()
+        if(this.parentElement.classList.contains('active')) {
+            this.parentElement.classList.remove('active')
+        } else {
+            document.querySelectorAll('.conversation-item-dropdown').forEach(function(i) {
+                i.classList.remove('active') 
+            })
+            this.parentElement.classList.add('active')
+        }
+    })
+})
+
+document.addEventListener('click', function(e) {
+    if(!e.target.matches('.conversation-item-dropdown, .conversation-item-dropdown *')) {
+      document.querySelectorAll('.conversation-item-dropdown').forEach(function(i) {
+        i.classList.remove('active')
+      }) 
+    }
+})
+
+document.querySelector('.conversation-form-input').forEach(function(item) {
+    item.addEventListener('input', function() {
+        this.rows = this.value.split('\n').length;
+    })
+})
+
+document.querySelectorAll('[data-conversation]').forEach(function(item) {
+    item.addEventListener('click', function(e) {
+        e.preventDefault()
+        document.querySelectorAll('.conversation').forEach(function(i) {
+            i.classList.remove('active')
+        })
+        document.querySelector(this.dataset.conversation).classList.add('active')
+    })
+})
+
+document.querySelectorAll('.conversation-back').forEach(function(item) {
+    item.addEventListener('click', function(e) {
+        e.preventDefault()
+        this.closest('.conversation').classList.remove('active')
+        document.querySelector('.conversation-default').classList.add('active')
+    })
+})
+// end: conversation
